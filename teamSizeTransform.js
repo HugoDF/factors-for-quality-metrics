d50afbe18b150ccd8917fce49d940386fa3a3490
HugoDF <hdifrancesco@gmail.com>
2016-12-06 13:28:35 +0000

    Add some JSON. Transform data. Get correlations.

---
+++ teamSizeTransform.js
@@ -0,0 +1,30 @@
+const stats = require('simple-statistics');
+const teamSizes = require('./teamSizes');
+
+const results = Object.keys(teamSizes).reduce( function(prev, key) {
+    const curr = teamSizes[key];
+
+    const computeFailureRate = curr.map( ([ name, total, fail ]) => {
+        return fail / total;
+    });
+
+    const sumFailureRates = computeFailureRate.reduce( (prev, curr) => (prev + curr), 0);
+
+    const numberOfRates = computeFailureRate.length;
+
+    const aggregateFailureRate = numberOfRates !== 0 ? (sumFailureRates / numberOfRates) : 0;
+
+    // if(aggregateFailureRate !== 0) {
+        prev[key] = aggregateFailureRate;
+    // }
+
+    return prev;
+}, {});
+
+const sizes = Object.keys(results);
+const failureRate = Object.values(results);
+
+const correlation = stats.sampleCorrelation(sizes, failureRate);
+
+// console.log(correlation);
+console.log(JSON.stringify([ sizes.map(Number), failureRate ]));
---
+++ visualise.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+from numpy import corrcoef
+
+import json
+
+def plot(xs, ys):
+    plt.plot(xs,ys)
+    plt.show()
+
+
+with open('sizeVsFailureRate.json') as data_file:
+    data = json.load(data_file)
+    sizes, failureRate = data
+    plot(sizes, failureRate)
